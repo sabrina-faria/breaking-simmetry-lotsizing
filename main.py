d8960673f9bbedba29f2ac1a1d8e20f18a7f179a
sabrina <sabrinaferreira123@hotmail.com>
2024-10-09 15:13:41 -0300

    nova correção do experimento

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ if __name__ == "__main__":
     }
 
     # Defina quais formulações deseja rodar usando números
-    formulations_to_run = [1]  # Altere essa lista conforme necessário
+    formulations_to_run = [0,1,2,3,4,5,6,7]  # Altere essa lista conforme necessário
 
-    for num in [0,1,2,3,4,5,6,7]:
+    for num in [1]:
         context = ProjectContext(f"experimentos/experimento{num}.yml", num)
 
         for formulation_number in formulations_to_run:
