c6a2386f3d60ddd30e7b407846d13393b48dd153
sabrina <sabrinaferreira123@hotmail.com>
2025-11-12 23:57:59 -0400

    conferindo intancia do exp 3

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     }
 
     # Defina quais formulações deseja rodar usando números
-    formulations_to_run = [0,7]  # Altere essa lista conforme necessário
+    formulations_to_run = [7]  # Altere essa lista conforme necessário
 
     for num in [3]:
         context = ProjectContext(f"experimentos/experimento{num}.yml", num)
